75306fe3f8e5f331b5b1b5387c19d4976ea754c9
지우 <emily1290@naver.com>
2025-09-05 10:17:49 +0900

    Solved: [그래프 탐색] BOJ_단어 변환 홍지우

---
+++ 그래프 탐색/지우/PG_단어 변환.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+queue<pair<string,int>> q;
+vector<bool> vis;
+vector<string> wrds;
+string t;
+
+bool Diff1(string a, string b) {
+    int cnt = 0;
+    for(int i=0; i<a.length(); i++) {
+        if(a[i] != b[i]) cnt++;
+    }
+    
+    if(cnt==1) return true;
+    return false;
+}
+
+int bfs(string begin) {
+    q.push({begin, 0});
+    
+    while(!q.empty()) {
+        auto [curr, cnt] = q.front(); q.pop();
+        
+        if(curr == t) return cnt;
+        
+        for(int i=0; i<wrds.size(); i++) {
+            if(!vis[i] && Diff1(wrds[i], curr)) {
+                vis[i] = true;
+                q.push({wrds[i], cnt+1});
+             }
+        }
+    }
+    
+    return 0;
+}
+
+int solution(string begin, string target, vector<string> words) {
+    vis.resize(words.size(), false);
+    wrds = words;
+    t = target;
+    
+    return bfs(begin);
+}
