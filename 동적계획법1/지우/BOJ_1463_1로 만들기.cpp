007f74ed5885935e7733e1db2cbfcaf565bfd91a
지우 <emily1290@naver.com>
2025-11-05 10:39:41 +0900

    Solved: [동적계획법1] BOJ_1로 만들기 홍지우

---
+++ 동적계획법1/지우/BOJ_1463_1로 만들기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int INF = 98765432;
+
+int main() {
+    int N; cin >> N;
+    vector<int> dp(1000001, INF);
+
+    dp[1] = 0; dp[2] = 1; dp[3] = 1;
+    for(int i=4; i<=N; i++) {
+        if(i%3 == 0) dp[i] = dp[i/3]+1;
+        if(i%2 == 0) dp[i] = min(dp[i], dp[i/2]+1);
+        dp[i] = min(dp[i], dp[i-1]+1);
+    }
+    cout << dp[N];
+    return 0;
+}
